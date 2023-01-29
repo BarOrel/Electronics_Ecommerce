598e5124f6e5d97d8c8af6fa9bd14176b971e41f
Bar Orel <bari0777@walla.com>
2023-01-29 13:43:25 +0200

    Added Things

--- Electronics-client/src/app/components/Add-Folder/Add-Televsion/Add-Televsion.component.ts
+++ Electronics-client/src/app/components/Add-Folder/Add-Televsion/Add-Televsion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-Add-Televsion',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Add-Televsion.component.css']
 })
 export class AddTelevsionComponent implements OnInit {
-
+  @Output() Storage: EventEmitter<any> = new EventEmitter<any>();
+  @Output() InchSize: EventEmitter<any> = new EventEmitter<any>();
+  @Output() Miliamper: EventEmitter<any> = new EventEmitter<any>();
+  @Output() OperationSystem: EventEmitter<any> = new EventEmitter<any>();
+  @Output() Resolution: EventEmitter<any> = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit() {
   }
+  SelectedStorage(value: any) { this.Storage.emit(value) }
 
+  MilliamperHoursNum(value: any) { this.Miliamper.emit(value) }
+
+  InchNum(value: any) { this.InchSize.emit(value) }
+
+  selectedoperationSystem(value: any) { this.OperationSystem.emit(value) }
+
+  selectedResolutaion(value: any) { this.Resolution.emit(value) }
 }
